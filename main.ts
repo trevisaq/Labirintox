b2a826ba827a09f8f5704fa1f8d1f866404fe8b9
trevisaq <souzagui2t@gmail.com>
2023-11-14 16:21:11 -0300

    Just New Update

---
+++ main.ts
@@ -0,0 +1,120 @@
+const color = require('cli-color');
+const input = require('prompt-sync')();
+
+//matriz planta 1
+
+//matriz planta 2
+
+const colorir = (text: number): string => {
+    if (text == 22) {
+        return color.red.bgRed('  ')
+    } else if (text == 11) {
+        return color.blue.bgBlue('  ')
+    } else if (text == 33) {
+        return color.green.bgGreen('  ')
+    } else {
+        return color.yellow.bgYellow('  ')
+    }
+}
+
+var x: number = 0;
+var y: number = 5;
+var passos: number = 0;
+
+const mostrarPlanta = (planta_atual: number[][]): void => {
+    console.clear();
+    for (let i of planta_atual) {
+        let linha: any[] = [];
+        for (let e of i) {
+            linha.push(colorir(e))
+        }
+        console.log(linha.toString().replaceAll(',', ''))
+    }
+    console.log('Passos: ', passos);
+}
+
+const descer = (planta:number[][]): void => {
+    if (x + 1 < planta.length && planta[x + 1][y] !== 22) {
+        planta[x][y] = 11;
+        x = x + 1;
+        planta[x][y] = 33;
+        passos++;
+    }
+}
+
+const subir = (planta:number[][]): void => {
+    if (x - 1 >= 0 && planta[x - 1][y] !== 22) {
+        planta[x][y] = 11;
+        x = x - 1;
+        planta[x][y] = 33;
+        passos++;
+    }
+}
+
+const esquerda = (planta:number[][]): void => {
+    if (y - 1 >= 0 && planta[x][y - 1] !== 22) {
+        planta[x][y] = 11;
+        y = y - 1;
+        planta[x][y] = 33;
+        passos++;
+    }
+}
+
+const direita = (planta:number[][]): void => {
+    if (y + 1 < planta[0].length && planta[x][y + 1] !== 22) {
+        planta[x][y] = 11;
+        y = y + 1;
+        planta[x][y] = 33;
+        passos++;
+    }
+}
+
+const verificaProduto = (): boolean => {
+    for (var linha of planta) {
+        for (var item of linha) {
+            if (item == 88) {
+                return false;
+            }
+        }
+    }
+    return true;
+
+    
+}
+
+const verificarPlanta = (planta_atual:number[][]): boolean =>{
+    if (planta_atual == planta){
+        return true
+    }else {
+        return false
+    }
+}
+const start = (planta_atual:number[][]) => {
+    mostrarPlanta(planta_atual);
+    let escolha: string = input('Digite uma direção (w, a, s, d) ou (q) para sair: ');
+    if (escolha.toUpperCase() == 'Q' || verificaProduto()) {
+        return
+    } else if (escolha.toUpperCase() == 'W') {
+        subir(planta_atual)
+    } else if (escolha.toUpperCase() == 'A') {
+        esquerda(planta_atual);
+    } else if (escolha.toUpperCase() == 'S') {
+        descer(planta_atual);
+    } else if (escolha.toUpperCase() == 'D') {
+        direita(planta_atual);
+    }
+    if(x == 8 && y == 19 && verificarPlanta(planta_atual)){
+        x = 2;
+        y = 0;
+        planta_atual = planta2
+    } else if(x == 0 && y == 5 && verificarPlanta(planta_atual) != true){
+        x = 0;
+        y = 5;
+        planta[0][5] = 33
+        planta[8][19] = 11
+        planta_atual = planta
+    }
+    start(planta_atual)
+}
+
+start(planta)
